5a2ee1d42f65f8e0bfd18a767c478cdcb1d19227
vkovalev <vd.kv@ya.ru>
2020-04-27 20:57:34 +0300

    fix cmake config for tests

--- test/test-1.sh
+++ test/test-1.sh
@@ -3,5 +3,5 @@
 CMD=$1
 shift
 for arg do
-    $CMD -1 $arg | diff -u --from-file ${arg}.eta.ur - || exit 1
+    $CMD -1 $arg | diff -u --from-file ${arg}.eta.1 - || exit 1
 done
--- test/test-13.sh
+++ test/test-13.sh
@@ -3,5 +3,5 @@
 CMD=$1
 shift
 for arg do
-    $CMD -13 $arg | diff -u --from-file ${arg}.eta - || exit 1
+    $CMD -13 $arg | diff -u --from-file ${arg}.eta.13 - || exit 1
 done
