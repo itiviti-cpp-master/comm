81656e4f1e7a0553d7d795262b9b8a16670ed2d4
vkovalev <vd.kv@ya.ru>
2020-04-27 21:04:20 +0300

    Fix test call scripts

--- test/test-2.sh
+++ test/test-2.sh
@@ -3,5 +3,5 @@
 CMD=$1
 shift
 for arg do
-    $CMD -2 $arg | diff -u --from-file ${arg}.eta.2 - || exit 1
+    $CMD -2 $arg ${arg}_2 | diff -u --from-file ${arg}.eta.2 - || exit 1
 done
--- test/test.sh
+++ test/test.sh
@@ -3,5 +3,5 @@
 CMD=$1
 shift
 for arg do
-    $CMD $arg | diff -u --from-file ${arg}.eta - || exit 1
+    $CMD $arg ${arg}_2 | diff -u --from-file ${arg}.eta - || exit 1
 done
