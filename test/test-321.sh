71f38f41c30a8b78459af8e436146773f9bc72f8
Oratovskii_Oleg <oleg_oratovskiy@mail.ru>
2022-04-23 22:41:44 +0300

    added new line to CMakeLists.txt ; in test-321.sh changed 123->321

--- test/test-321.sh
+++ test/test-321.sh
@@ -3,5 +3,5 @@
 CMD=$1
 shift
 for arg do
-    $CMD -123 $arg ${arg}_2 | diff -u --from-file ${arg}.eta.123 - || exit 1
+    $CMD -123 $arg ${arg}_2 | diff -u --from-file ${arg}.eta.321 - || exit 1
 done
